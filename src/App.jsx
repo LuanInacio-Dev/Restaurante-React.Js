7d638957fd11f3aba1b2d696b0da02151766fd86
Luan Inacio <94462233+luaninacio755@users.noreply.github.com>
2024-02-01 18:14:55 -0400

    Add files via upload
    npm run dev

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import Hashtaurante from "./assets/hashtaurante.webp";
+import './App.css';
+import { Navegacao } from "./Navegacao";
+import { ItemCardapio } from "./ItemCardapio";
+import { pratosPrincipais ,sobremesas ,bebidas } from "./cardapio";
+
+
+export function App(){ //Disponivel pro js
+  const paginasmenu = [pratosPrincipais ,sobremesas,bebidas]
+  const [paginaSelecionada,atualizarPaginaSelecionada]= useState(0);
+
+
+ 
+  return <>
+  <img src= {Hashtaurante} alt="" className="capa"/>
+  
+   <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
+   <div className="menu">
+   
+   {paginasmenu[paginaSelecionada].map(item => <ItemCardapio 
+   nome={item.nome} 
+   descricao ={item.descricao}
+   preco ={item.preco}
+   imagem ={item.imagem}
+
+   />)}
+ 
+   </div>
+  </>
+}
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import {App} from './App.jsx'
+import {App} from './App.jsx' //Importa elementos da minha pagina 
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
